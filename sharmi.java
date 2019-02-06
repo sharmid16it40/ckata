45a1727c756cadf3a0483276973f4729ebb28f0f
sharmid16it40 <47380221+sharmid16it40@users.noreply.github.com>
2019-02-06 13:35:55 +0530

    Create sharmi.java

---
+++ sharmi.java
@@ -0,0 +1,19 @@
+public class positive
+{
+    public static void main(String[] args) 
+    {
+        int number=3;
+        if(number > 0)
+        {
+            System.out.println(positive");
+        }
+        else if(number < 0)
+        {
+            System.out.println(negative);
+        }
+        else
+        {
+            System.out.println(" zero");
+        }
+    }
+}
